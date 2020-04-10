7cd93acb212c6f84dc571938aae842784b1aeece
Camille Cerin <camille.cerin@gmail.com>
2020-04-10 17:14:18 +0200

    added: /fonts landing.phpscss fonts.scss

--- views/landing.php
+++ views/landing.php
@@ -8,5 +8,15 @@
 </head>
 <body>
     <?php  require("components/header_landpage.php") ?>
+
+    <section class="hero">
+        <article class="hero_title">
+            <h1>WATCH YOUR MOVIES IN ONE PLACE</h1>
+        </article>
+        <article class="hero_infos">
+            <p>Start your free trial and watch thousands of movies wherever you want starting at <em>6.99$</em>/month</p>
+        </article>
+    </section>
+    
 </body>
 </html>
