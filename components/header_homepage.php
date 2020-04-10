228692935d3c3ef3c72681d85352cbe756ca0c7e
Nawel Berrichi <nawel.berrichi@hetic.net>
2020-04-10 22:21:35 +0200

    add header for homepage

---
+++ components/header_homepage.php
@@ -0,0 +1,22 @@
+<header class="headerHome">
+
+    <div class="headerHome__logo">
+        <img src="assets/icon/logo_svg.png" alt="logo_lumino">
+    </div>
+    <div class="headerHome__nav nav">
+      <div class="nav__item">Home</div>
+      <div class="nav__item">Genre <img class="item__arrow" src="assets/icon/chevron_gris.svg"/> </div>
+      <div class="nav__item">My list</div>
+    </div>
+
+    <div class="headerHome__search">
+      <img src="assets/icon/loupe.svg"/>
+      <div class="search__text"> Search a title</div>
+    </div>
+
+    <div class="headerHome__bell"><img src="assets/icon/bell.svg"/></div>
+    <div class="headerHome__name">Hello Hugo !</div>
+    <div class="headerHome__profil"><img src="assets/img/profil.png"/></div>
+    <div class="headerHome__disconnect"><img src="assets/icon/unlog.svg"/></div>
+
+</header>
--- controller/controller.php
+++ controller/controller.php
@@ -22,5 +22,4 @@ function getError($email){
 }
 
 
-
 ?>
