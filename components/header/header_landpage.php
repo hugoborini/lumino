8ae8a73c1b2c1b8c68fed3cb78bdd2dd6496e7a8
hugo borini <hborini@localhost.localdomain>
2020-04-10 04:56:07 -0400

    {add} header landpage in component :penguin:

---
+++ components/header/header_landpage.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <div class ="container-image">
+        <img src="assets/logo_svg.png" alt="logo_lumino">
+        </div>
+        <div class="button">
+            <button class="button__connexion">LOG IN</button>
+            <button class="button__connexion">SIGN UP</button>
+        </div>
+    </header>
+</body>
+</html>
