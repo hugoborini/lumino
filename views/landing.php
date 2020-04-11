540742d826ff0a7e685ef3fb998496259d519547
Camille Cerin <camille.cerin@gmail.com>
2020-04-11 10:32:22 +0200

    hero landing page: ok

--- views/landing.php
+++ views/landing.php
@@ -11,14 +11,34 @@
 
 
     <section class="hero">
-        <article class="hero_title">
+        <article class="hero__title">
             <h1>WATCH YOUR MOVIES IN ONE PLACE</h1>
         </article>
-        <article class="hero_infos">
+        <article class="hero__infos">
             <p>Start your free trial and watch thousands of movies wherever you want starting at <em>6.99$</em>/month</p>
+        </article>
+        <article class="hero__CTA">
+                START YOUR FREE TRIAL
         </article>
     </section>
     
+<section class="hero__bottom">
+    <svg class="downIcon" width="30" height="12" viewBox="0 0 30 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M1 1L15 11L29 1" stroke="#f0f1f3" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>
+</section>
+
+<section class="watch">
+    <article>
+    <p class="watch__title">Watch movies wherever, wheneveryou want.</p>
+    <p class="watch__body">On your desktop, on your laptop, your tablet or your phone, we are available everywhere, on any device. 
+No need for internet connection to watch your favorite movies, juste download them and whatch them later.</p>
+    </article>
+
+    <article class="mockup">
+        <img src="assets/img/mockups.png" alt="" class="mockups">
+    </article>
+</section>
 
     <?php  require("components/footer.php") ?>
 
