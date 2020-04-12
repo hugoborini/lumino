d617bfec2900fa724468062e69d33b00fc81db9f
Klochette <melina.chamayou@gmail.com>
2020-04-12 19:52:54 +0200

    gradient on video

--- components/main_movie.php
+++ components/main_movie.php
@@ -1,5 +1,6 @@
 <section class="main__movie">
     <div class="category__main">
+      <div class="video__wrapper"></div>
       <div class="category__video"><video autoplay="true" loop muted src="./assets/video/1917.mp4">Votre navigateur ne supporte pas la vidéo</video></div>
       <div class="category__path">Genre > <span>Action</span> </div>
 
