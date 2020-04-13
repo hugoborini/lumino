9c74f9d97b3df2a690e6184e78ebadc7a1b2f90d
Nawel Berrichi <nawel.berrichi@hetic.net>
2020-04-13 17:16:38 +0200

    modified player

--- index.php
+++ index.php
@@ -40,8 +40,8 @@ require("controller/controller.php");
         }
     }
     else {
-        require("views/landing.php");
-        //require("views/player.php");
+        //require("views/landing.php");
+        require("views/player.php");
         //require("views/genre.php");
     }
 ?>
